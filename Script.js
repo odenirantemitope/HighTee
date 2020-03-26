a44e0a6fc4f2692cf0ef73efb5c8688fc0f1eb76
odenirantemitope <54293057+odenirantemitope@users.noreply.github.com>
2020-03-26 22:03:15 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+//Name display in a variable
+var name="odeniran temitope"
+ console.log(name)
+//store courses in an array
+var courses=["Html", "CSS", "JS"];
+  console.log(courses)
+//If the number of courses you are doing is an odd number,find and display all odd numbers from 1-200
+for (var i = 1; i <=200; i++){
+	if(i % 2 !==0){
+	console.log(i);
+	}
+}
